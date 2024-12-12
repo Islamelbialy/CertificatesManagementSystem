351422e43496d04f3de1611c46a9c99e3a6518cc
islamelbialy <islam.mohamed@mans.edu.eg>
2024-12-12 14:14:15 +0200

    dfe

--- CertificatesManagementSystem/settings.py
+++ CertificatesManagementSystem/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'certificates',
-    'Foundations'
+    'Foundations',
     'registeration',
     'widget_tweaks',
     'Programs'
